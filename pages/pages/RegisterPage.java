76b3aeb8d08f54bf1505c3bee881c29129d8a94f
Gholami-Bahareh <baharehgholamii86@gmail.com>
2025-09-11 02:08:13 +0200

    register page init

--- pages/pages/RegisterPage.java
+++ pages/pages/RegisterPage.java
@@ -1,10 +1,99 @@
 package pages;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class RegisterPage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	WebDriver driver;
+    WebDriverWait wait;
+    
+    //constructor
+    public RegisterPage(WebDriver driver ) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));  
 
-	}
+        }
+    
+//پر کردن فرم با داده‌های درست و ثبت موفقیت‌آمیز
+//بررسی اینکه فیلدهای ضروری خالی باشن و خطا نمایش داده بشه
+//بررسی اعتبار ایمیل یا پسورد (مثلاً کوتاه بودن یا بدون کاراکتر خاص)
+//انتخاب جنسیت و اطمینان از اینکه سیستم انتخاب رو درست ثبت می‌کنه
+//بررسی اینکه دکمه ثبت فعال یا غیر فعال باشه در شرایط مختلف
+//تست رفتار فرم بعد از ثبت (مثل پیام موفقیت یا ریدایرکت)
+    
+    // Locators
+    By maleGenderRadio = By.id("gender-male");
+    By femaleGenderRadio = By.id("gender-female");
+    By firstnameField = By.id("FirstName");
+    By lastnameField = By.id("LastName");
+    By emailField = By.id("Email");
+    By passwordField = By.id("Password");
+    By confirmpasswordField = By.id("ConfirmPassword");
+    By registerButton = By.id("register-button");
+    By resultText = By.xpath("//div[@class='result']");
+    
+    //Method
+    
+    public void selectMaleGender() {
+        WebElement malegenderBox = wait.until(ExpectedConditions.visibilityOfElementLocated(maleGenderRadio));
+        malegenderBox.click();  
+    }
+    
+    public void selectFemaleGender() {
+        WebElement femalegenderBox = wait.until(ExpectedConditions.visibilityOfElementLocated(femaleGenderRadio));
+        femalegenderBox.click();  
+    }
+    
+    public void enterFirstName(String firstname) {
+        WebElement firstnameBox = wait.until(ExpectedConditions.visibilityOfElementLocated(firstnameField));
+        firstnameBox.clear();
+        firstnameBox.sendKeys(firstname);   
+    }
+    
+    public void enterLastName(String lastname) {
+        WebElement lastnameBox = wait.until(ExpectedConditions.visibilityOfElementLocated(lastnameField));
+        lastnameBox.clear();
+        lastnameBox.sendKeys(lastname);   
+    }
+    
+    public void enterEmail(String email) {
+           WebElement emailBox = wait.until(ExpectedConditions.visibilityOfElementLocated(emailField));
+           emailBox.clear();
+           emailBox.sendKeys(email);   
+       }
+    
+    public void enterPassword(String password) {
+        WebElement passwordBox = wait.until(ExpectedConditions.visibilityOfElementLocated(passwordField));
+        passwordBox.clear();
+        passwordBox.sendKeys(password);   
+    }
+    
+    public void enterConfirmPassword(String confirmpassword) {
+        WebElement confirmpasswordBox = wait.until(ExpectedConditions.visibilityOfElementLocated(confirmpasswordField));
+        confirmpasswordBox.clear();
+        confirmpasswordBox.sendKeys(confirmpassword);   
+    }
+    
+    public void clickRegister() {
+        WebElement registerBox = wait.until(ExpectedConditions.visibilityOfElementLocated(registerButton));
+        registerBox.click();  
+        
+    }
+    
+    public boolean seeSuccessMessage() {
+    	try {
+        WebElement successText = wait.until(ExpectedConditions.visibilityOfElementLocated(resultText));
+        return successText.isDisplayed();   
+    	} catch (Exception e) {
+            return false; 
+        }
+    }
+    }
 
-}
+
---
+++ tests/tests/RegisterTest.java
@@ -0,0 +1,50 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import pages.HomePage;
+import pages.RegisterPage;
+
+public class RegisterTest {
+	 WebDriver driver;
+	 WebDriverWait wait;
+	 RegisterPage registerpage;
+	 
+	 
+	 @BeforeClass
+	    public void setup() {
+	        System.setProperty("webdriver.chrome.driver","D:\\chromedriver-win64\\chromedriver.exe");
+	        driver = new ChromeDriver();
+	        driver.get("https://demowebshop.tricentis.com/register");
+	        registerpage = new RegisterPage(driver);
+	        driver.manage().window().maximize();
+	    }
+	
+	 @Test
+	    public void validsuccessfulRegister() {
+		 registerpage.selectMaleGender();
+		 registerpage.enterFirstName("rrr");
+		 registerpage.enterLastName("eee");
+		 registerpage.enterEmail("bnh.r2m2@gmail.com");
+		 registerpage.enterPassword("225588");
+		 registerpage.enterConfirmPassword("225588");
+		 registerpage.clickRegister();
+		 
+		 Assert.assertTrue(registerpage.seeSuccessMessage(),"be successfull");
+	             
+    }
+	 
+	 @AfterClass
+	    public void teardown() {
+	        driver.quit();
+	    }
+	
+    
+
+}
