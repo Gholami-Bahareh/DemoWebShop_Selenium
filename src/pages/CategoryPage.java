c3f0299c8fae2fe93c8a0472fe4a44185201336a
Gholami-Bahareh <baharehgholamii86@gmail.com>
2025-09-12 23:51:29 +0200

    categoryPageinit!

--- src/pages/CategoryPage.java
+++ src/pages/CategoryPage.java
@@ -1,10 +1,35 @@
 package pages;
 
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class CategoryPage {
+	WebDriver driver;
+    WebDriverWait wait;
+    
+    //constructor
+    public CategoryPage(WebDriver driver ) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));  
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        }
+    
+    
+ // Locator ساده برای لیست محصولات
+    By products = By.cssSelector(".product-item");
 
-	}
-
+    // متد برای گرفتن تعداد محصولات
+    public int getNumberOfProducts() {
+        List<WebElement> productList = driver.findElements(products);
+        return productList.size();
+    }
+    
+    // Locators
+    // Methods
+	
 }
